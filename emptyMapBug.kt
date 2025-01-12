f0fb64627eb67c51b4d83c5e9530ecadd63f6719
BugHunter-X <askzenai@gmail.com>
2025-01-12 07:32:06 +0100

    Add emptyMapBug.kt file

---
+++ emptyMapBug.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val list = listOf(1, 2, 3, 4, 5)
+    val result = list.map { it * 2 }
+    println(result) // Output: [2, 4, 6, 8, 10]
+
+    val emptyList = emptyList<Int>()
+    val emptyResult = emptyList.map { it * 2 } 
+    println(emptyResult) //Output: []
+}
